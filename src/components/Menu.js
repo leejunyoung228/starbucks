d4447c94c7be243d039404cd1f51879d17120695
leejunyoung228 <junyeongi748@gmail.com>
2022-06-23 12:14:52 +0900

    6th commit

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -18,11 +18,26 @@ export default function Menu() {
           <span className="in_span">상세분류</span>
         </p>
       </div>
+      <div className="butt">
+        <dt class="dtp">
+          <a role="button" className="a1 selected">
+            {" "}
+            <img src="./images/icon_pic_on.png" /> 사진으로 보기
+          </a>
+        </dt>
+        <dt class="dtp2">
+          <a role="button" className="a2">
+            {" "}
+            <img src="./images/icon_ante.png" /> 영양정보로 보기
+          </a>
+        </dt>
+      </div>
       <div>
         <dl>
           <dt>
               <a>콜드 브루 커피</a>
               <img className="sumIcon" src="./images/logo_decaf.png" />
+              <br/>
               <p className="summary">
                 디카페인 에스프레소 샷 추가 가능 (일부 음료 제외)
               </p>
